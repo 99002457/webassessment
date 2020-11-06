d1b522c85b905239e2ceb965bc23f75dc2198036
99002457 <chethan.gc@ltts.com>
2020-11-06 12:04:39 +0530

    Last commit

---
+++ Question 4/app.js
@@ -0,0 +1,57 @@
+    var express= require('express');
+    var app= express();
+    var hotelList= require('./hotel.json')
+
+    var cors = function(req, res, next) { 
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+        next(); 
+    } 
+    app.use(cors);
+
+
+    app.get("/showAll",function(req,res){
+        res.status(200).send(hotelList)
+    })
+
+    app.get("/showByCity",function(req,res){
+        var ht= req.query.city;
+        var newhotelList=[];
+        hotelList.forEach(hotel => {
+            if(hotel.City==ht)
+            {
+                newhotelList.push(hotel)
+            }
+        });
+        res.status(200).send(newhotelList);
+    })
+
+    app.get("/showByType",function(req,res){
+        var ht= req.query.Type;
+        var newhotelList=[];
+        hotelList.forEach(hotel => {
+            if(hotel.type=ht)
+            {
+                newhotelList.push(hotel)
+            }
+        });
+        res.status(200).send(newhotelList);
+    })
+    app.get("/showByCuisine",function(req,res){
+        var ht= req.query.Type;
+        var newhotelList=[];
+        hotelList.forEach(hotel => {
+            if(hotel.type=ht && isArray(hotel))
+            {
+                 hotel.forEach(element => {
+                    if(element==ht)
+                    {newhotelList.push(hotel)}
+                });
+            }
+        });
+        res.status(200).send(newhotelList);
+    })
+ 
+ 
+    app.listen(3004,function(){
+        console.log("Server started in the post 3004")
+    })
